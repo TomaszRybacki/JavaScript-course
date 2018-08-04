4baf87127eceae88dd2fcb33bcb13443b913e5a9
Tomasz89 <TomaszRybacki1989@gmail.com>
2018-08-04 16:58:31 +0200

    page scrolling added

--- scripts/code.js
+++ scripts/code.js
@@ -1,3 +1,6 @@
+// Menu animation
+
+
 var menuButtonElem = document.getElementById('menu-button');
 var navigationElem = document.getElementById('navbar');
 
@@ -13,3 +16,36 @@ window.onload = function() {
     navigationElem.classList.toggle('navigation--active');
   }
 }
+
+
+// Page scrolling
+
+
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            return false;
+          } else {
+            $target.attr('tabindex','-1');
+            $target.focus();
+          }
+        });
+      }
+    }
+  });
